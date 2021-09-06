7f312690907543d77bba2ed501cbd3e0c9780a39
ea2016 <enriquesuarez1991@gmail.com>
2021-09-06 14:35:34 +0200

    a

--- src/main/java/com/formacionbdi/springboot/app/servicios/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/servicios/SpringbootServicioProductosApplication.java
@@ -1,4 +1,4 @@
-package com.formacionbdi.springboot.app.productos;
+package com.formacionbdi.springboot.app.servicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
